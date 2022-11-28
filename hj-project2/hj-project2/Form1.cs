69cc3c67f28296044706499df6512208624f18fd
현지 <93240906+aicul313@users.noreply.github.com>
2022-11-28 18:50:20 +0900

    아두이노 C# 시리얼 연계
    인터넷 찾아보긴 했는데 아직두 어렵다 C#은...

---
+++ hj-project2/hj-project2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace hj_project2
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort1 = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "")
+            {
+                return;
+            }
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.DataBits = 8;
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.Open();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connect Error", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            button1.Text = serialPort1.IsOpen ? "Disconnection" : "Connection";
+            comboBox1.Enabled = !serialPort1.IsOpen;
+        }
+
+        // LED 불빛 켜기
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                return;
+            }
+            serialPort1.Write("1");
+        }
+
+        
+        // LED 불빛 끄기
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                return;
+            }
+            serialPort1.Write("0");
+        }
+        
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            foreach (var item in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+    }
+}
